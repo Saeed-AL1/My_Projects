2b202afe19828528ba81eaaba1853c29c2bd28ee
Saeed ALi <saym544@gmail.com>
2019-10-25 03:27:51 -0400

    Removed some code

--- script.py
+++ script.py
@@ -1,22 +1,5 @@
-import math
-import sys
-from os import rename
-
-# from os import rename
 import requests
 
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Saeed'))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-# name = input("Your Name? ")
-# print("Hello,", name)
+print(r.ok)
